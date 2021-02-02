887e65cc9ca10de2675368f39f17e5abfbd46a23
Guille <ggalmazor@gmail.com>
2021-02-02 12:10:37 +0100

    First commit

---
+++ test/gilded_rose_test.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/gilded_rose'
+require "minitest/autorun"
+
+class TestGildedRose < Minitest::Test
+
+  def test_hookup
+    assert true
+  end
+
+  def test_foo
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].name, "foo"
+  end
+
+end
